625faa764a9bdade80456ed4a4e57b4691f2ef78
Mohsin11-argon <mohsin.khan@argonteq.com>
2026-02-03 12:05:12 +0500

    error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
+// export default defineConfig({
+//   root: 'src',
+//   build: {
+//     outDir: '../dist',
+//     emptyOutDir: true,
+//   },
+//   plugins: [vue()],
+//   server: {
+//     port: 3000,
+//   },
+// });
+
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  root: 'src',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: 'dist',
+    sourcemap: false,
   },
-  plugins: [vue()],
   server: {
-    port: 3000,
+    port: 5173,
   },
 });
